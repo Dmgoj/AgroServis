af3cb435ed66af4a5602de980c90f5174f442384
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-01-02 23:34:13 +0100

    Add IMaintenanceServiceInterface

---
+++ AgroServis.Services/IMaintenanceService.cs
@@ -0,0 +1,21 @@
+﻿using AgroServis.Services.DTO;
+using AgroServis.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.Services
+{
+    public interface IMaintenanceService
+    {
+        Task<PagedResult<MaintenanceDto>> GetAllAsync(int page, int pageSize);
+        Task<MaintenanceDto> GetByIdAsync(int id);
+        Task<MaintenanceEditDto> GetByIdForEditAsync(int id);
+        Task<MaintenanceEditDto> GetForCreateAsync();
+        Task<int> CreateAsync(MaintenanceCreateDto dto);
+        Task UpdateAsync(MaintenanceUpdateDto dto);
+        Task DeleteAsync(int id);
+    }
+}
