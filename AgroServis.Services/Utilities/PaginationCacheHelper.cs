15da937de43cfb13ad9502777b825f71e6e3f142
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-01-08 22:06:46 +0100

    Move InvalidatePaginationCaches method to PaginationCacheHelper class and use the method in Equipment and Maintenance Service

---
+++ AgroServis.Services/Utilities/PaginationCacheHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.Services.Utilities
+{
+    public static class CacheHelper
+    {
+        public static void InvalidatePaginationCaches(
+            IMemoryCache cache,
+            ILogger logger,
+            string entityName,
+            int maxPages = 10,
+            int[] pageSizes = null)
+        {
+            pageSizes ??= new[] { 10, 20, 50 };
+
+            logger.LogDebug("Invalidating {EntityName} pagination caches", entityName);
+
+            foreach (var pageSize in pageSizes)
+            {
+                for (int page = 1; page <= maxPages; page++)
+                {
+                    var cacheKey = $"{entityName}Page_{page}_Size_{pageSize}";
+                    cache.Remove(cacheKey);
+                }
+            }
+        }
+    }
+}
