b1d9b10f06db06391823b1a52fd8c4804b9aedcc
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-01-12 23:18:49 +0100

    Add migration AddDeletedAtpropertyInMaintenance and global query filter for DeletedAt for Maintenance Entity

--- AgroServis.DAL/ApplicationDbContext.cs
+++ AgroServis.DAL/ApplicationDbContext.cs
@@ -7,13 +7,18 @@ namespace AgroServis.DAL
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         public DbSet<EquipmentType> EquipmentTypes { get; set; }
         public DbSet<Equipment> Equipment { get; set; }
         public DbSet<EquipmentCategory> EquipmentCategories { get; set; }
         public DbSet<Maintenance> MaintenanceRecords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Maintenance>().HasQueryFilter(m => m.DeletedAt == null);
+        }
     }
 }
