56971e47a314522667dd86e6776f2135d8492e14
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-01-27 23:43:19 +0100

    Add RoleSeeder

---
+++ AgroServis.DAL/Seeding/RoleSeeder.cs
@@ -0,0 +1,43 @@
+﻿using AgroServis.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.DAL.Seeding
+{
+    public class RoleSeeder
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleSeeder(
+            RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task SeedAsync()
+        {
+            // Create roles
+            await CreateRolesAsync();
+        }
+
+        private async Task CreateRolesAsync()
+        {
+            string[] roleNames = { "Admin", "Worker" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await _roleManager.RoleExistsAsync(roleName))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
