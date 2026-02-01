46a927c87993276a8c5db0c88fcd874ee1a0f996
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-01 22:14:19 +0100

    Add CreateWorkerDto

---
+++ AgroServis.Services/DTO/CreateWorkerDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public record CreateWorkerDto
+{
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "Passwords don't match")]
+    public string ConfirmPassword { get; set; }
+
+    [Phone]
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber { get; set; }
+
+    public string Position { get; set; }
+}
