c16716996aa3dd8ac451400fbed67f317c5ba5ca
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-02 23:30:50 +0100

    Add WorkerDto

---
+++ AgroServis.Services/DTO/WorkerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AgroServis.Services.DTO;
+
+public record WorkerDto
+{
+    public int Id { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string FullName => $"{FirstName} {LastName}";
+    public string Email { get; init; }
+    public string? PhoneNumber { get; init; }
+    public string? Position { get; init; }
+    public string UserId { get; init; }
+}
