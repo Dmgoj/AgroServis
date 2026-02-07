20b7e53354e49d4cf96b52d8eb7ba08cc7613280
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-07 15:34:01 +0100

    Add IEmailService interface

---
+++ AgroServis.Services/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using AgroServis.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.Services
+{
+    public interface IEmailService
+    {
+        Task SendAdminApprovalNotificationAsync(PendingRegistration registration);
+        Task SendApprovalConfirmationAsync(string email, string firstName);
+        Task SendRejectionNotificationAsync(string email, string firstName);
+    }
+}
