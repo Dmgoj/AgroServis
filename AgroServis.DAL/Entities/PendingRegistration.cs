7987c571e959f46a80f0ff11d03c35a2760c4371
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-07 15:33:19 +0100

    Add PendingRegistration entity for temporary storing new user data before it gets approved by Admin

---
+++ AgroServis.DAL/Entities/PendingRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.DAL.Entities
+{
+    public class PendingRegistration
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Position { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public bool IsProcessed { get; set; } = false;
+    }
+}
