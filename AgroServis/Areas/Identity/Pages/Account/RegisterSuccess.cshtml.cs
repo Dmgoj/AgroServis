4fde1321e4754199f84ce11ec35df4935ee82d5c
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-09 23:24:21 +0100

    Add RegisterSuccessModel

---
+++ AgroServis/Areas/Identity/Pages/Account/RegisterSuccess.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroServis.Areas.Identity.Pages.Account
+{
+    public class RegisterSuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
