b8ff9b676ef24338fa161439dcc737ef39d67f2f
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-13 23:31:51 +0100

    Add the AddApprovalTokenToPendingRegistrations migration

---
+++ AgroServis.DAL/Migrations/20260213221957_AddApprovalTokenToPendingRegistration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroServis.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApprovalTokenToPendingRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovalToken",
+                table: "PendingRegistrations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpiresAt",
+                table: "PendingRegistrations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalToken",
+                table: "PendingRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "TokenExpiresAt",
+                table: "PendingRegistrations");
+        }
+    }
+}
