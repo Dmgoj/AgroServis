9a6dbf62e0d8d7fa632f63a08354a1189d46ecf3
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-20 23:51:53 +0100

    Add migration AddMaintenancePerformedByUserFK

--- AgroServis.DAL/Migrations/20260220223632_AddMaintenancePerformedByUserFK.Designer.cs
+++ AgroServis.DAL/Migrations/20260220223632_AddMaintenancePerformedByUserFK.Designer.cs
@@ -4,6 +4,7 @@ using AgroServis.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AgroServis.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260220223632_AddMaintenancePerformedByUserFK")]
+    partial class AddMaintenancePerformedByUserFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AgroServis.DAL/Migrations/20260220223632_AddMaintenancePerformedByUserFK.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroServis.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaintenancePerformedByUserFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PerformedBy",
+                table: "MaintenanceRecords",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaintenanceRecords_PerformedBy",
+                table: "MaintenanceRecords",
+                column: "PerformedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MaintenanceRecords_AspNetUsers_PerformedBy",
+                table: "MaintenanceRecords",
+                column: "PerformedBy",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MaintenanceRecords_AspNetUsers_PerformedBy",
+                table: "MaintenanceRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MaintenanceRecords_PerformedBy",
+                table: "MaintenanceRecords");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PerformedBy",
+                table: "MaintenanceRecords",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
