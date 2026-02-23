23fd018bc62315014b308fc779a499903138a43f
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-23 23:08:56 +0100

    Add migration AddCompletedAtPropertyToMaintenanceEntity

---
+++ AgroServis.DAL/Migrations/20260223215332_AddCompletedAtPropertyToMaintenance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroServis.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompletedAtPropertyToMaintenance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAt",
+                table: "MaintenanceRecords",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedAt",
+                table: "MaintenanceRecords");
+        }
+    }
+}
