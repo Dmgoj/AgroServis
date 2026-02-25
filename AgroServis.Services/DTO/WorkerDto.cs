228e1c2d72200a153d48d18bfe0c827fd1237a3c
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-25 22:20:18 +0100

    Add default value to the Email property of WorkerDto

--- AgroServis.Services/DTO/WorkerDto.cs
+++ AgroServis.Services/DTO/WorkerDto.cs
@@ -6,7 +6,7 @@ public record WorkerDto
     public string FirstName { get; init; }
     public string LastName { get; init; }
     public string FullName => $"{FirstName} {LastName}";
-    public string Email { get; init; }
+    public string Email { get; init; } = "";
     public string? PhoneNumber { get; init; }
     public string? Position { get; init; }
     public string UserId { get; init; }
