7e826c16ecdb080f7a20378201bcafe537e673a9
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-26 23:40:22 +0100

    Add WorkerUpdateDto for Editing workers

---
+++ AgroServis.Services/DTO/WorkerUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AgroServis.Services.DTO;
+
+public record WorkerUpdateDto
+{
+    public int Id { get; init; }
+    public string FirstName { get; init; } = "";
+    public string LastName { get; init; } = "";
+    public string Email { get; init; } = "";
+    public string? PhoneNumber { get; init; }
+    public string? Position { get; init; }
+}
