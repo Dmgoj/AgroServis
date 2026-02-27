7ddde6b17ca982ac411e8c9305007ed5afe495c1
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-02-27 23:50:38 +0100

    Add IPdfReportService interface

---
+++ AgroServis.Services/IPdfReportService.cs
@@ -0,0 +1,14 @@
+﻿using AgroServis.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.Services
+{
+    public interface IPdfReportService
+    {
+        byte[] GenerateMaintenanceReport(IEnumerable<MaintenanceDto> items);
+    }
+}
