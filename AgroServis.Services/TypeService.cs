75a021e816650a460d6f1f4bb32691954d0f8a41
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-03-07 22:36:44 +0100

    Implement ITypeService in TypeService

--- AgroServis.Services/TypeService.cs
+++ AgroServis.Services/TypeService.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace AgroServis.Services
 {
-    internal class TypeService
+    internal class TypeService : ITypeService
     {
+        public Task<TypeDto> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TypeDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(TypeDto category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TypeDto category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
