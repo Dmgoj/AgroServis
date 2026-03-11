daa4fe3a7993491d6aae46f9169d0132a948561f
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-03-11 23:40:29 +0100

    Add MaintenanceSchedule entity

---
+++ AgroServis.DAL/Entities/MaintenanceSchedule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.DAL.Entities
+{
+    public class MaintenanceSchedule
+    {
+        public int Id { get; set; }
+
+        public int EquipmentId { get; set; }
+        public Equipment Equipment { get; set; } = null!;
+
+        public string Title { get; set; } = "";
+        public string? Description { get; set; }
+
+        public int IntervalDays { get; set; }
+
+        public DateTime? LastPerformedAt { get; set; }
+        public DateTime NextDueDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
