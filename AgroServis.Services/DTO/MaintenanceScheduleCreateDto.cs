87eec2577473090cf12a03548767ccd0434fc221
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-03-12 22:47:03 +0100

    Add MaintenanceScheduleCreateDto

---
+++ AgroServis.Services/DTO/MaintenanceScheduleCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AgroServis.Services.DTO
+{
+    public record MaintenanceScheduleCreateDto
+    {
+        public int EquipmentId { get; init; }
+        public string Title { get; init; } = "";
+        public string? Description { get; init; }
+        public int IntervalDays { get; init; }
+        public DateTime? LastPerformedAt { get; init; }
+    }
+}
