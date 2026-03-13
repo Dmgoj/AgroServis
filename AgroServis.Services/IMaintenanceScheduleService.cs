f7dffdb58ac50cc064519ea878c66de649c6fa91
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-03-13 22:53:44 +0100

    Addi IMaintenanceScheduleService interface

---
+++ AgroServis.Services/IMaintenanceScheduleService.cs
@@ -0,0 +1,22 @@
+﻿using AgroServis.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroServis.Services
+{
+    public interface IMaintenanceScheduleService
+    {
+        Task<int> CreateAsync(MaintenanceScheduleCreateDto dto);
+
+        Task<List<MaintenanceScheduleDto>> GetAllAsync();
+
+        Task<MaintenanceScheduleDto?> GetByIdAsync(int id);
+
+        Task UpdateAsync(MaintenanceScheduleUpdateDto dto);
+
+        Task DeleteAsync(int id);
+    }
+}
