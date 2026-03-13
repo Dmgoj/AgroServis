bbb5c4e7ca949aad8b8cd88d87bf765b9a776a1b
Domagoj Grgić <domagoj.grgic@outlook.com>
2026-03-13 22:54:55 +0100

    Add MaintenanceSchedulesController

---
+++ AgroServis/Controllers/MaintenanceSchedulesController.cs
@@ -0,0 +1,21 @@
+﻿using AgroServis.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroServis.Controllers
+{
+    public class MaintenanceSchedulesController : Controller
+    {
+        private readonly IMaintenanceScheduleService _service;
+
+        public MaintenanceSchedulesController(IMaintenanceScheduleService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var schedules = await _service.GetAllAsync();
+            return View(schedules);
+        }
+    }
+}
